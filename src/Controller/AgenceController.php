8d4578a39beeb83c372d3f85d57a0857afb28d6d
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-04-08 02:56:53 +0100

    afficher user

--- src/Controller/AgenceController.php
+++ src/Controller/AgenceController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AgenceRepository;
+
 
 class AgenceController extends AbstractController
 {
@@ -15,6 +17,20 @@ class AgenceController extends AbstractController
     {
         return $this->render('agence/index.html.twig', [
             'controller_name' => 'AgenceController',
+        ]);
+    }
+
+    /**
+     * @param AgenceRepository $repository
+     * @return Response
+     * @Route ({"/","/afficheAgence"},name="afficheAgence")
+     */
+    public function afficheAgence(AgenceRepository $repository)
+    {
+        //$repository=$this->getDoctrine()->getRepository(Classroom::class);
+        $tabuser = $repository->findAll();
+        return $this->render('agence/index.html.twig', [
+            'tab' => $tabuser
         ]);
     }
 }
